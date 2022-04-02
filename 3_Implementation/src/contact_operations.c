208d09d368b98a660c5b276ce6993e9d01308003
giriprakashk <101519714+giriprakashk@users.noreply.github.com>
2022-04-03 01:31:56 +0530

    commit

---
+++ 3_Implementation/src/contact_operations.c
@@ -0,0 +1,20 @@
+/**
+ * @file contact_operations.c
+ * @author Giri Prakash K (e8ec179@sairamtap.edu.in)
+ * @brief This is the source code which contains all the functions for the project
+ * @version 0.1
+ * @date 2022-03-26
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <contact_operations.h>
+
+/**
+ * @brief Shows the record of contact
+ * @return Text file generated
+ */
+int main()
+{
+    return ("contact.dll","r");
+}
