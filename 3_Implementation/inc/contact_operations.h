103c8c618bb1e8c2e7644750f4c16a148a197459
giriprakashk <101519714+giriprakashk@users.noreply.github.com>
2022-04-03 01:23:09 +0530

    commit

---
+++ 3_Implementation/inc/contact_operations.h
@@ -0,0 +1,20 @@
+/**
+ * @file contact_operations.h
+ * @author Giri Prakash K (e8ec179@sairamtap.edu.in)
+ * @brief Header file for Contact Management System
+ * @version 0.1
+ * @date 2022-03-26
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef CONTACT_MANAGEMENT_SYSTEM_H_
+#define CONTACT_MANAGEMENT_SYSTEM_H_
+
+/**
+ * @brief Shows the record of contacts
+ * @return Text file generated 
+ */
+int main();
+
+#endif /* #define CONTACT_MANAGEMENT_SYSTEM_H_ */
