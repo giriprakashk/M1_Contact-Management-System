a4e1d48061902f64d85941483fda8a0b6e051b04
giriprakashk <101519714+giriprakashk@users.noreply.github.com>
2022-04-02 22:28:17 +0530

    Create Contact-Management-System.h

---
+++ 3_Implementation/inc/Contact-Management-System.h
@@ -0,0 +1,15 @@
+#ifndef CONTACT_MANAGEMENT_SYSTEM_H_INCLUDED
+#define CONTACT_MANAGEMENT_SYSTEM_H_INCLUDED
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<process.h>
+#include<stdlib.h>
+#include<dos.h>
+list;
+char query[20],name[20];
+FILE *fp, *ft;
+int i,n,ch,l,found;
+
+
+#endif // CONTACT_MANAGEMENT_SYSTEM_H_INCLUDED
